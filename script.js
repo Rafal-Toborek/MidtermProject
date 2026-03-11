e7d3755451a0c29ff24a50b93ac6a3ecbae235ac
Rafal Toborek <rtoborek@my.waketech.edu>
2026-03-11 14:02:10 -0400

    day 2

--- script.js
+++ script.js
@@ -4,75 +4,118 @@ let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("prev");
 let toOutside = document.getElementById("toOutside");
 let toPorch = document.getElementById("toPorch");
+let toLHallway = document.getElementById("toLHallway")
+let toRHallway = document.getElementById("toRHallway")
+let backToLiving = document.getElementById("backToLiving")
+let toTheAttic = document.getElementById("toTheAttic")
 
-images = ["./images/Call2Action.png","./images/Outside.png","./images/Around.png","./images/Porch.png" ,"./images/Living.png", "./images/LHallway.png","./images/RHallway.png"];
+images = ["./images/Call2Action.png","./images/Outside.png","./images/Around.png","./images/Porch.png" ,"./images/Living.png", "./images/LHallway.png","./images/Kitchen.png","./images/Creepy People.png","./images/Insanity Ending.png","./images/RHallway.png"];
 sceneText = ["Around", "Call to Action", "Outside", "Living Room", "Porch","Left Hallway", "Right Hallway"];
 
 
 
 let currentScene = 0;
 text.textContent = sceneText[0];
-
-
-if (currentScene === 0) {
-    toOutside.style.display = "none";
-    toPorch.style.display = "none";
-    nextButton.style.display = "inline-block";
-    prevButton.style.display = "inline-block";
-    nextButton.addEventListener("click", function() {
-        currentScene++;
-        scene.src = images[currentScene];
-        text.textContent = sceneText[currentScene];
-        console.log(currentScene);
-        });
+function updateScene() {
+    scene.src = images[currentScene];
+    text.textContent = sceneText[currentScene];
+ 
+    if (currentScene === 0) {
+        toOutside.style.display = "none";
+        toPorch.style.display = "none";
+        toLHallway.style.display = "none";
+        toRHallway.style.display = "none";
+        nextButton.style.display = "inline-block";
+        prevButton.style.display = "inline-block";
+        backToLiving.style.display = "none"
+ 
     } else if (currentScene === 1) {
         toOutside.style.display = "inline-block";
         toPorch.style.display = "inline-block";
+            toLHallway.style.display = "none";
+        toRHallway.style.display = "none";
         nextButton.style.display = "none";
         prevButton.style.display = "inline-block";
-        console.log(currentScene);
-        toOutside.addEventListener("click", function() {
-            currentScene = 2;
-            scene.src = images[currentScene];
-            text.textContent = sceneText[currentScene];
-            toOutside.style.display = "none";
-            toPorch.style.display = "none";
-            nextButton.style.display = "inline-block";
-            prevButton.style.display = "inline-block";
-            console.log(currentScene);
-            });
-        toPorch.addEventListener("click", function() {
-            currentScene = 3;
-            scene.src = images[currentScene];
-            text.textContent = sceneText[currentScene];
-            toOutside.style.display = "none";
-            toPorch.style.display = "none";
-            nextButton.style.display = "inline-block";
-            prevButton.style.display = "inline-block";
-            console.log(currentScene);
-            });
-        } else if (currentScene === 3 || currentScene === 2) {
-            toOutside.style.display = "inline-block";
-            toPorch.style.display = "inline-block";
-            nextButton.addEventListener("click", function() {
-                currentScene = 4;
-                    scene.src = images[currentScene];
-                    text.textContent = sceneText[currentScene];
-                    toOutside.style.display = "none";
-                    toPorch.style.display = "none";
-                        
-            });
-        }
-
-
-
-console.log(currentScene);
+        backToLiving.style.display = "none"
+    } else if (currentScene === 2 || currentScene === 3) {
+        toOutside.style.display = "none";
+        toPorch.style.display = "none";
+        toLHallway.style.display = "none";
+        toRHallway.style.display = "none";
+        nextButton.style.display = "inline-block";
+        prevButton.style.display = "inline-block";
+        backToLiving.style.display = "none"
+    } else if (currentScene === 4) {
+        toOutside.style.display = "none";
+        toPorch.style.display = "none";
+        toLHallway.style.display = "inline-block";
+        toRHallway.style.display = "inline-block";
+        nextButton.style.display = "none";
+        prevButton.style.display = "none";
+        backToLiving.style.display = "none"
+    } else if (currentScene === 5) {
+        toOutside.style.display = "none";
+        toPorch.style.display = "none";
+        toLHallway.style.display = "none";
+        toRHallway.style.display = "none";
+        nextButton.style.display = "inline-block";
+        prevButton.style.display = "none";
+        backToLiving.style.display = "inline-block"
+    } else if (currentScene === 9) {
+        toOutside.style.display = "none";
+        toPorch.style.display = "none";
+        toLHallway.style.display = "none";
+        toRHallway.style.display = "none";
+        nextButton.style.display = "inline-block";
+        prevButton.style.display = "none";
+        backToLiving.style.display = "inline-block";
+    }
+    
+    console.log(currentScene);
+}
+ 
+// Event listeners just update currentScene and call the function
+nextButton.addEventListener("click", function() {
+    currentScene++;
+    updateScene();
+});
+ 
 prevButton.addEventListener("click", function() {
     if (currentScene > 0) {
         currentScene--;
-        scene.src = images[currentScene];
-        console.log(currentScene);
+        updateScene();
     }
 });
+ 
+toOutside.addEventListener("click", function() {
+    currentScene = 2;
+    updateScene();
+});
+ 
+toPorch.addEventListener("click", function() {
+    currentScene = 3;
+    updateScene();
+});
+ 
+toLHallway.addEventListener("click", function(){
+    currentScene = 5
+    updateScene()
+});
+toRHallway.addEventListener("click", function(){
+    currentScene = 9
+    updateScene()
+});
+
+backToLiving.addEventListener("click", function(){
+    currentScene = 4
+    updateScene()
+})
+toTheAttic.addEventListener("click", function(){
+
+    
+}
+
+)
 
 console.log(currentScene)
+updateScene()
